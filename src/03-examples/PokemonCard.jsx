18e2b7f91d6977516100be15e2363556a4e8b49f
JesusPineda29 <jesuspineda1429@gmail.com>
2025-05-23 10:35:02 -0600

    creando nuevo componente memo

--- src/03-examples/PokemonCard.jsx
+++ src/03-examples/PokemonCard.jsx
@@ -1,10 +1,21 @@
-
+import { useLayoutEffect, useRef } from 'react';
 
 export const PokemonCard = ({ id, name, sprites = {} }) => {
+
+  const h2Ref = useRef();
+
+  useLayoutEffect(() => {
+
+    const { height, width } = h2Ref.current.getBoundingClientRect();
+    console.log(height, width);
+  }
+  , []);
+
+
   return (
-    <div className="card text-center shadow-sm mb-4" style={{ width: '18rem', height: 'auto' }}>
+    <div className="card text-center shadow-sm mb-4" style={{ display: 'flex', width: '18rem', height: '200', flexDirection: 'row' }}>
       <div className="card-header bg-primary text-white">
-        <h5 className="mb-0 text-capitalize">#{id} - {name}</h5>
+        <h2 ref={h2Ref} className="mb-0 text-capitalize">#{id} - {name}</h2>
       </div>
 
       <div className="card-body d-flex justify-content-center align-items-center" style={{ height: '200px' }}>
--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,14 @@ import './index.css'
 //import { CounterApp } from './01-useState/CounterApp'
 //import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 //import { HooksApp } from './HooksApp'
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+//import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+//import { FocusScreen } from './04-useRef/FocusScreen'
+//import { Layout } from './05-useLayoutEffect/Layout'
+import { Memorize } from './06-memos/Memorize'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MultipleCustomHooks />
+    <Memorize  />
   </StrictMode>,
 )
